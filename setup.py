cb353c9fb9a19b9e529d2a3cb9183bb8d9c3c5df
unknown <hu649@purdue.edu>
2022-08-29 01:21:51 -0700

    Make fastformerLM a module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='leap-transformer',
-    version='0.1.0',
+    version='0.1.1',
     license='CC0 1.0 Universal',
     author='Michael Hu',
     author_email='prmhu@yahoo.com',
---
+++ src/leap/fastformerLM/__init__.py
@@ -0,0 +1,1 @@
+from .fastformer import FastformerForCausalLM, FastformerLMConfig
