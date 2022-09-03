574dd92c12191be186791d764c1d228d19ffa3f2
unknown <hu649@purdue.edu>
2022-09-03 12:57:34 -0700

    0.1.2 release

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
 
+
+# put a disclaimer in the readme for publishing on pypi
+description = open("README.md", "r", encoding="utf-8").read().split("\n")
+description.insert(2, "NOTE: The description shown here is just the github README. As such, some of the equations may not render. Checkout out the github for a more refined description where you can also see the code and contribute!")
+description.insert(3, '')
+
+
+
 setup(
     name='leap-transformer',
-    version='0.1.1',
+    version='0.1.2',
     license='CC0 1.0 Universal',
     author='Michael Hu',
     author_email='prmhu@yahoo.com',
@@ -13,8 +21,8 @@ setup(
     ),
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
+    long_description='\n'.join(description),
+    long_description_content_type='text/markdown',
     keywords='linear transformer NLP deep learning pytorch',
     install_requires=[
         'torch>=1.0.0',
